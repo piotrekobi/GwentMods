3c661a115978d629f48b550e8e3e39d782d4abd0
Jester <gaymer56channel@gmail.com>
2026-03-04 18:14:14 +0500

    Added on/off switch for boardify mod

--- Boardify/Boardify.cs
+++ Boardify/Boardify.cs
@@ -3,6 +3,7 @@ using Il2CppGwentGameplay;
 using Il2CppGwentUnity;
 using MelonLoader;
 using ModSettings.TranslationProviders;
+using System.Xml.Linq;
 
 [assembly: MelonInfo(typeof(Boardify.Boardify), "Boardify", "1.0.0", "Jester")]
 [assembly: MelonGame("CDProjektRED", "Gwent")]
@@ -10,39 +11,71 @@ namespace Boardify;
 
 public class Boardify : MelonMod
 {
+    const string ModId = "Boardify";
     internal static MelonPreferences_Entry<string> boardPreference = null!;
     private static string? pendingBoard;
 
+    internal static MelonPreferences_Entry<bool> isModEnabledPreference = null!;
+    private static string? pendingEnable;
+
     public override void OnInitializeMelon()
     {
-        boardPreference = MelonPreferences.CreateCategory("Boardify").CreateEntry("Boardify", BoardId.Yamurlak.ToString());
-        RegisterAllBoards(new EmbeddedFileTranslationProvider(MelonAssembly.Assembly, "Boardify.BoardTranslations.json"));
+        isModEnabledPreference = MelonPreferences.CreateCategory("Boardify").CreateEntry("BoardifyEnabled", true);
+        boardPreference = MelonPreferences.CreateCategory("Boardify").CreateEntry("CurrentBoard", BoardId.DandelionMeadow.ToString());
+        var translationProvider = new EmbeddedFileTranslationProvider(MelonAssembly.Assembly, "Boardify.BoardTranslations.json");
+        RegisterEnableSwitch(translationProvider);
+        RegisterAllBoards(translationProvider);
         HarmonyInstance.PatchAll();
+    }
+
+    private static void RegisterEnableSwitch(ITranslationProvider translationProvider)
+    {
+        string translationKey = "Boardify_Enabled_Translation";
+        ModSettings.ModSettings.RegisterTranslationKey(ModId, translationKey, translationProvider.GetTranslations(translationKey));
+        ModSettings.ModSettings.RegisterTranslationKey(ModId, true.ToString(), translationProvider.GetTranslations(true.ToString()));
+        ModSettings.ModSettings.RegisterTranslationKey(ModId, false.ToString(), translationProvider.GetTranslations(false.ToString()));
+        
+        var switcherOptions = new List<Tuple<string, Func<string>>>
+        {
+            Tuple.Create(true.ToString(), () => true.ToString()),
+            Tuple.Create(false.ToString(), () => false.ToString()),
+        };
+
+        ModSettings.ModSettings.RegisterSwitcherSetting(
+            modId: ModId,
+            uniqueSettingId: "BoardifyEnabled",
+            displayTranslationKey: translationKey,
+            switcherOptions: switcherOptions,
+            getCurrentValue: () => isModEnabledPreference.Value.ToString(), // currently saved value
+            onValueChangedCallback: val => pendingEnable = val as string != isModEnabledPreference.Value.ToString() ? val as string : null, // user changed the switcher in UI
+            hasPendingChangesCallback: () => pendingEnable != null, // are there unsaved changes?
+            applyPendingChangesCallback: () => { if (pendingEnable != null) { isModEnabledPreference.Value = bool.Parse(pendingEnable); pendingEnable = null; } }, // user clicked Save
+            revertPendingChangesCallback: () => pendingEnable = null); // user clicked Back/Cancel
     }
 
     private static void RegisterAllBoards(ITranslationProvider translationProvider)
     {
-        var options = new List<Tuple<string, Func<string>>>();
+        var translationKey = "Current_Board_Translation";
+        ModSettings.ModSettings.RegisterTranslationKey(ModId, translationKey, translationProvider.GetTranslations(translationKey));
 
+        var switcherOptions = new List<Tuple<string, Func<string>>>();
         foreach (BoardId board in Enum.GetValues(typeof(BoardId)))
         {
             string name = board.ToString();
-            ModSettings.ModSettings.RegisterTranslationKey("Boardify", name, translationProvider.GetTranslations(name));
-            options.Add(Tuple.Create(name, () => name));
+            ModSettings.ModSettings.RegisterTranslationKey(ModId, name, translationProvider.GetTranslations(name));
+            switcherOptions.Add(Tuple.Create(name, () => name));
         }
 
-        // Register switcher once with all enum options
         ModSettings.ModSettings.RegisterSwitcherSetting(
-            modId: "Boardify",
-            settingKey: "Boardify",
-            displayNameKey: "Boardify",
-            switcherOptions: options,
+            modId: ModId,
+            uniqueSettingId: "CurrentBoard",
+            displayTranslationKey: translationKey,
+            switcherOptions: switcherOptions,
             getCurrentValue: () => boardPreference.Value, // what's currently saved
             onValueChangedCallback: val => pendingBoard = val as string != boardPreference.Value ? val as string : null, // user changed the switcher in UI
             hasPendingChangesCallback: () => pendingBoard != null, // are there unsaved changes?
             applyPendingChangesCallback: () => { if (pendingBoard != null) { boardPreference.Value = pendingBoard; pendingBoard = null; } }, // user clicked Save
-            revertPendingChangesCallback: () => pendingBoard = null // user clicked Back/Cancel
-        );
+            revertPendingChangesCallback: () => pendingBoard = null); // user clicked Back/Cancel
     }
 }
 
@@ -61,8 +94,15 @@ public static class Patch_LoadBoard
             int desiredBoard = (int)Enum.Parse(typeof(BoardId), Boardify.boardPreference.Value);
             if (definition.ArtId != desiredBoard)
             {
-                definition.ArtId = desiredBoard;
-                MelonLogger.Msg($"Forced Board ArtId to {desiredBoard}");
+                if (Boardify.isModEnabledPreference.Value == true)
+                {
+                    definition.ArtId = desiredBoard;
+                    MelonLogger.Msg($"Forced Board ArtId to {desiredBoard}");
+                }
+                else
+                {
+                    MelonLogger.Msg($"But mod is disabled");
+                }
             }
             else
             {
