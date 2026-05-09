9f5ba30cce3b3211dc25c4f173cd1d33bb27476e
Jester <gaymer56channel@gmail.com>
2026-05-09 19:02:08 +0500

    Added hotkeys

--- VolumeHotkeyMod/VolumeHotkeyMod.cs
+++ VolumeHotkeyMod/VolumeHotkeyMod.cs
@@ -4,6 +4,7 @@ using Il2CppGwentUnity;
 using MelonLoader;
 using ModSettings;
 using ModSettings.TranslationProviders;
+using UnityEngine;
 
 [assembly: MelonInfo(typeof(VolumeHotkeyMod.VolumeHotkeyMod), "VolumeHotkeyMod", "1.0.0", "Jester")]
 [assembly: MelonGame("CDProjektRED", "Gwent")]
@@ -40,4 +41,51 @@ public class VolumeHotkeyMod : MelonMod
             applyPendingChangesCallback: () => { if (pendingMode != null) { modEnabledPreference.Value = pendingMode; pendingMode = null; } }, // user clicked Save
             revertPendingChangesCallback: () => pendingMode = null); // user clicked Back/Cancel
     }
+
+    public override void OnUpdate()
+    {
+        if (modEnabledPreference.Value != true.ToString())
+            return;
+
+        for (int i = 0; i < NumberKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(NumberKeys[i]))
+            {
+                SetVolume(i * 0.1f);
+                break;
+            }
+        }
+    }
+
+    private static void SetVolume(float volume)
+    {
+        var broadcaster = EventBroadcaster.Instance;
+        if (broadcaster == null)
+        {
+            MelonLogger.Warning("[VolumeHotkeyMod] EventBroadcaster instance not available.");
+            return;
+        }
+
+        string volumeStr = volume.ToString("F1", System.Globalization.CultureInfo.InvariantCulture);
+
+        broadcaster.SettingsChanged.Invoke(SettingsKey.MUSIC, volumeStr);
+        broadcaster.SettingsChanged.Invoke(SettingsKey.SFX, volumeStr);
+        broadcaster.SettingsChanged.Invoke(SettingsKey.VOICE, volumeStr);
+
+        MelonLogger.Msg($"[VolumeHotkeyMod] Volume set to {volume * 100:0}%");
+    }
+
+    private static readonly KeyCode[] NumberKeys = new KeyCode[]
+    {
+            KeyCode.Alpha0,
+            KeyCode.Alpha1,
+            KeyCode.Alpha2,
+            KeyCode.Alpha3,
+            KeyCode.Alpha4,
+            KeyCode.Alpha5,
+            KeyCode.Alpha6,
+            KeyCode.Alpha7,
+            KeyCode.Alpha8,
+            KeyCode.Alpha9,
+    };
 }
