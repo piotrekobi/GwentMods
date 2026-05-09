2e2aadd2877f4db82591b7b3301bfd9cf72b7c4a
Jester <gaymer56channel@gmail.com>
2026-05-09 19:19:34 +0500

    Add affected settings + mute buttons

--- VolumeHotkeyMod/VolumeHotkeyMod.cs
+++ VolumeHotkeyMod/VolumeHotkeyMod.cs
@@ -19,9 +19,10 @@ public class VolumeHotkeyMod : MelonMod
     public override void OnInitializeMelon()
     {
         modEnabledPreference = MelonPreferences.CreateCategory(ModId).CreateEntry("VolumeHotkeyMod_Enabled", true.ToString());
-        affectedVolumesPreference = MelonPreferences.CreateCategory(ModId).CreateEntry("AffectedVolumes", "Music");
+        affectedVolumesPreference = MelonPreferences.CreateCategory(ModId).CreateEntry("VolumeHotkeyMod_AffectedVolumes", "Music");
         var translationProvider = new EmbeddedFileTranslationProvider(MelonAssembly.Assembly, "VolumeHotkeyMod.ModTranslations.json");
         RegisterEnableSwitch(translationProvider);
+        RegisterAffectedVolumesSwitch(translationProvider);
         HarmonyInstance.PatchAll();
     }
 
@@ -30,7 +31,7 @@ public class VolumeHotkeyMod : MelonMod
         string? pendingMode = null;
         ModSettingsMod.RegisterSwitcherSetting(
             modId: ModId,
-            settingTranslationKey: ModSettingsMod.RegisterTranslationKey(ModId, "VolumeHotkeyMod_Enabled_Translation", translationProvider.GetTranslationsFor("VolumeHotkeyMod_Enabled_Translation")),
+            settingTranslationKey: ModSettingsMod.RegisterTranslationKey(ModId, "Mod_Enabled_Translation", translationProvider.GetTranslationsFor("Mod_Enabled_Translation")),
             switcherOptions: new List<string> {
                 ModSettingsMod.RegisterTranslationKey(ModId, true.ToString(), translationProvider.GetTranslationsFor(true.ToString())),
                 ModSettingsMod.RegisterTranslationKey(ModId, false.ToString(), translationProvider.GetTranslationsFor(false.ToString())),
@@ -40,6 +41,28 @@ public class VolumeHotkeyMod : MelonMod
             hasPendingChangesCallback: () => pendingMode != null, // are there unsaved changes?
             applyPendingChangesCallback: () => { if (pendingMode != null) { modEnabledPreference.Value = pendingMode; pendingMode = null; } }, // user clicked Save
             revertPendingChangesCallback: () => pendingMode = null); // user clicked Back/Cancel
+    }
+
+    private static void RegisterAffectedVolumesSwitch(TranslationProvider translationProvider)
+    {
+        string? pendingMode = null;
+        ModSettingsMod.RegisterSwitcherSetting(
+            modId: ModId,
+            settingTranslationKey: ModSettingsMod.RegisterTranslationKey(ModId, "AffectedVolumes_Translation", translationProvider.GetTranslationsFor("AffectedVolumes_Translation")),
+            switcherOptions: new List<string> {
+                ModSettingsMod.RegisterTranslationKey(ModId, "Music",             translationProvider.GetTranslationsFor("Music")),
+                ModSettingsMod.RegisterTranslationKey(ModId, "SFX",               translationProvider.GetTranslationsFor("SFX")),
+                ModSettingsMod.RegisterTranslationKey(ModId, "Speech",            translationProvider.GetTranslationsFor("Speech")),
+                ModSettingsMod.RegisterTranslationKey(ModId, "Music+SFX",         translationProvider.GetTranslationsFor("Music+SFX")),
+                ModSettingsMod.RegisterTranslationKey(ModId, "Music+Speech",      translationProvider.GetTranslationsFor("Music+Speech")),
+                ModSettingsMod.RegisterTranslationKey(ModId, "SFX+Speech",        translationProvider.GetTranslationsFor("SFX+Speech")),
+                ModSettingsMod.RegisterTranslationKey(ModId, "Music+SFX+Speech",  translationProvider.GetTranslationsFor("Music+SFX+Speech")),
+            },
+            getCurrentValue: () => affectedVolumesPreference.Value,
+            onValueChangedCallback: val => pendingMode = val as string != affectedVolumesPreference.Value ? val as string : null,
+            hasPendingChangesCallback: () => pendingMode != null,
+            applyPendingChangesCallback: () => { if (pendingMode != null) { affectedVolumesPreference.Value = pendingMode; pendingMode = null; } },
+            revertPendingChangesCallback: () => pendingMode = null);
     }
 
     public override void OnUpdate()
@@ -52,8 +75,25 @@ public class VolumeHotkeyMod : MelonMod
             if (Input.GetKeyDown(NumberKeys[i]))
             {
                 SetVolume(i * 0.1f);
-                break;
+                return;
             }
+        }
+
+        var broadcaster = EventBroadcaster.Instance;
+        if (broadcaster == null)
+        {
+            MelonLogger.Warning("[VolumeHotkeyMod] EventBroadcaster instance not available.");
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadPlus) || Input.GetKeyDown(KeyCode.Plus))
+        {
+            broadcaster.SettingsChanged.Invoke(SettingsKey.MUTE, false.ToString());
+            MelonLogger.Msg("[VolumeHotkeyMod] Unmuted");
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus) || Input.GetKeyDown(KeyCode.Minus))
+        {
+            broadcaster.SettingsChanged.Invoke(SettingsKey.MUTE, true.ToString());
+            MelonLogger.Msg("[VolumeHotkeyMod] Muted");
         }
     }
 
@@ -67,12 +107,16 @@ public class VolumeHotkeyMod : MelonMod
         }
 
         string volumeStr = volume.ToString("F1", System.Globalization.CultureInfo.InvariantCulture);
+        string mode = affectedVolumesPreference.Value;
 
-        broadcaster.SettingsChanged.Invoke(SettingsKey.MUSIC, volumeStr);
-        broadcaster.SettingsChanged.Invoke(SettingsKey.SFX, volumeStr);
-        broadcaster.SettingsChanged.Invoke(SettingsKey.VOICE, volumeStr);
+        if (mode == "Music" || mode == "Music+SFX" || mode == "Music+Speech" || mode == "Music+SFX+Speech")
+            broadcaster.SettingsChanged.Invoke(SettingsKey.MUSIC, volumeStr);
+        if (mode == "SFX" || mode == "Music+SFX" || mode == "SFX+Speech" || mode == "Music+SFX+Speech")
+            broadcaster.SettingsChanged.Invoke(SettingsKey.SFX, volumeStr);
+        if (mode == "Speech" || mode == "Music+Speech" || mode == "SFX+Speech" || mode == "Music+SFX+Speech")
+            broadcaster.SettingsChanged.Invoke(SettingsKey.VOICE, volumeStr);
 
-        MelonLogger.Msg($"[VolumeHotkeyMod] Volume set to {volume * 100:0}%");
+        MelonLogger.Msg($"[VolumeHotkeyMod] Volume set to {volume * 100:0}% (mode: {mode})");
     }
 
     private static readonly KeyCode[] NumberKeys = new KeyCode[]
